402bc7fc51ddf99d753ece0d441ff4c27ed9d9d9
joey22 <jsgillett@crimson.ua.edu>
2025-09-16 00:42:39 -0500

    feat: add 25-9-16 Non-Coprime Numbers Solution

---
+++ daily-problems/25-09-16-2197-Replace-Non-Coprime-Numbers.cpp
@@ -0,0 +1,26 @@
+/*
+Problem: Replace Non-Coprime Numbers in Array (LeetCode #2197)
+Given an array of integers replace every pair of adjacent non-coprime integers with their lcm.
+Performance: 
+- Runtime: 20 ms (Beats 58.80%)
+- Memory: 125.29 MB (Beats 44.44%)
+*/
+class Solution {
+public:
+
+    vector<int> replaceNonCoprimes(vector<int>& nums) {
+        vector<int> v; // Vector v functions as a stack holding the current array
+        int current;
+        for(int num : nums) {
+            current = num;
+            //While the top element is not coprime
+            while(!v.empty() && gcd(v.back(), current) > 1) {
+                //Merge the numbers with their lcm
+                current = std::lcm(v.back(), current);
+                v.pop_back(); 
+            }
+            v.push_back(current);
+        }
+        return v;
+    }
+};
